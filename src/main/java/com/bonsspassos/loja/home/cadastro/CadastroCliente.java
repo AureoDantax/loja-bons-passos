4fab3d2ff26347ddd3310e918729aa84b0f5d153
Ramon Garcia <ramonsantos.g@hotmail.com>
2024-03-20 14:03:23 -0300

    Adicionando cadastro de produtos e formatando Swing

--- src/main/java/com/bonsspassos/loja/home/cadastro/CadastroCliente.java
+++ src/main/java/com/bonsspassos/loja/home/cadastro/CadastroCliente.java
@@ -228,8 +228,7 @@ public class CadastroCliente extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
